e7d33d6d4953220f3ef7fa9636ce295664c05233
Geraldopereirads <geraldo.pereirads@hotmail.com>
2025-04-23 18:22:48 -0300

    feat: finish class account class

---
+++ Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectBank;
+
+public class Account
+{
+    public int Number { get; set; }
+    public string? Holder { get; set; }
+    public decimal Balance { get; set; } = 0.00m;
+
+
+
+    public decimal Deposit(decimal value)
+    {
+        Balance += value;
+        return Balance;
+    }
+
+
+    public decimal withdraw(decimal value)
+    {
+        Balance -= value;
+        return Balance;
+    }
+
+}
